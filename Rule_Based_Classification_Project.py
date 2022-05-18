57b6edb970aad67573e3ce3828b245bc3b7902f3
Eromer55 <85547094+Eromer55@users.noreply.github.com>
2022-05-18 10:54:10 +0300

    Update Rule_Based_Classification_Project.py

--- Rule_Based_Classification_Project.py
+++ Rule_Based_Classification_Project.py
@@ -47,7 +47,7 @@ agg_df = agg_df.reset_index()
 bins = [0, 18, 23, 30, 40, agg_df["AGE"].max()]
 
 # Bölünen noktalara karşılık isimlendirmelerin ne olacağını ifade edelim.
-my_labels = ['0_18', '19_23', '24-30', '31-40', '41_' + str(agg_df["AGE"].max())]
+my_labels = ['0_18', '19_23', '24_30', '31_40', '41_' + str(agg_df["AGE"].max())]
 agg_df['AGE_CAT'] = pd.cut(agg_df['AGE'], bins, labels=my_labels)
 
 # Yeni seviye tabanlı müşterileri (persona) tanımlamaya çalışalım.
@@ -72,5 +72,53 @@ agg_df.groupby("SEGMENT").agg({"PRICE" : ["mean", "max", "sum"]})
 
 
 # Yeni gelen müşterileri sınıflandırıp, ne kadar gelir getirebileceklerini tahmin ediniz.
-new_user = 'TUR_ANDROID_FEMALE_24-30'
+new_user = 'BRA_ANDROID_FEMALE_24_30'
 agg_df[agg_df["customers_level_based"] == new_user]
+
+# Bonus : Kullanıcıdan değer alarak potalsiyel müşteri segmentini ve getirisini hesaplayalım.
+print('*********** Kural Bazlı Potalsiyel Müşteri Getirisi Hesaplama ************')
+while True:
+  print()
+  print("Ülkeler : \n\n1.{0}\n2.{1}\n3.{2}\n4.{3}\n5.{4}\n".format((df['COUNTRY'].unique()[0].upper()), (df['COUNTRY'].unique()[1].upper()), (df['COUNTRY'].unique()[2].upper()), (df['COUNTRY'].unique()[3].upper()), (df['COUNTRY'].unique()[4].upper()), (df['COUNTRY'].unique()[5].upper())))
+  country = str(input("İstediğiniz ülkeyi yazınız ->\n "))
+  print("İşletim sistemleri : \n\n1.{0}\n2.{1}\n".format((df['SOURCE'].unique()[0].upper()), (df['SOURCE'].unique()[1].upper())))
+  source = str(input("İstediğiniz işletim sistemini yazınız -> \n"))
+  print("Cinsiyetler : \n\n1.{0}\n2.{1}\n".format((df['SEX'].unique()[0].upper()), (df['SEX'].unique()[1].upper())))
+  sex = str(input("İstediğiniz cinsiyeti yazınız -> \n"))
+  age = int(input("Yaşı giriniz  -> \n"))
+  if age < int(my_labels[0][2:]):
+      age = my_labels[0]
+    elif (age >= int(my_labels[0][2:])) & (age < int(my_labels[1][3:])):
+      age = my_labels[1]
+    elif (age >= int(my_labels[1][3:])) & (age < int(my_labels[2][3:])):
+        age = my_labels[2]
+    elif (age >= int(my_labels[2][3:])) & (age < int(my_labels[3][3:])):
+        age = my_labels[3]
+    else:
+        age = my_labels[4]
+  persona = (country.upper() + "_" + source.upper() + "_" + sex.upper() + "_" + age)
+  print(persona)
+  new_customer = agg_df[agg_df['customers_level_based'] == persona]
+  print(f"Segmenti : {str(new_customer.iloc[0]['SEGMENT'])}")
+  print(f"Potansiyel getirisi : {str(new_customer.iloc[0]['PRICE'])}")
+  break
+  
+# Bonus : Fonksiyon ile potansiyel getiriyi hesaplayalım.
+def calc_segment(dataframe, Country, Source, Sex, Age):
+    if Age < int(my_labels[0][2:]):
+        Age = my_labels[0]
+    elif (Age >= int(my_labels[0][2:])) & (Age < int(my_labels[1][3:])):
+        Age = my_labels[1]
+    elif (Age >= int(my_labels[1][3:])) & (Age < int(my_labels[2][3:])):
+        Age = my_labels[2]
+    elif (Age >= int(my_labels[2][3:])) & (Age < int(my_labels[3][3:])):
+        Age = my_labels[3]
+    else:
+        Age = my_labels[4]
+    Persona = (Country.upper() + "_" + Source.upper() + "_" + Sex.upper() + "_" + Age)
+    print(Persona)
+    New_Customer = agg_df[agg_df['customers_level_based'] == Persona]
+    print(f"Segmenti : {str(New_Customer.iloc[0]['SEGMENT'])}")
+    print(f"Potansiyel getirisi : {str(New_Customer.iloc[0]['PRICE'])}")
+
+calc_segment(agg_df, 'TUR', 'IOS', 'Male', 15)
